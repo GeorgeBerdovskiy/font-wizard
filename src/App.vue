68aecc64d865a50ad20e303cf5f4ddc286aa1ec0
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2022-12-11 00:22:54 -0800

    Add Search Field, Dropdown Button (Not Functional)

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,25 @@
 
 		<div class="content-container">
 		<div class="content">
-			<h1>Search Fonts</h1>
+
+			<div>
+				<h1 class="margin-bottom-half">Search Fonts</h1>
+
+				<div class="input-with-button margin-bottom-half">
+					<input type="text"/>
+					<button id="search-button">􀊫</button>
+				</div>
+
+				<label class="margin-bottom-quarter">Search Source</label>
+				<button class="secondary">
+					Google Fonts 
+
+					<div class="button-icon">
+						􀆈
+					</div>
+				</button>
+			</div>
+
 		</div>
 		</div>
 	</main>
@@ -70,6 +88,11 @@
 
     background-color: white;
     border-radius: 10px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
   }
 
   #logo {
@@ -118,5 +141,12 @@
   .navigation-button .text {
     padding-left: 16px;
     line-height: 40px;
+  }
+
+  #search-button {
+	width: 56px;
+	height: 56px;
+	font-size: 1.25rem;
+	color: white;
   }
 </style>
